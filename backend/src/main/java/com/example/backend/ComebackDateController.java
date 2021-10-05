6b60da88134b632839fe74d3ff44f8fdb05a9006
Zhiwei <sherman2zhiwei@yahoo.com>
2021-10-06 00:44:10 +0800

    Add cross-origin for local testing purposes

--- backend/src/main/java/com/example/backend/ComebackDateController.java
+++ backend/src/main/java/com/example/backend/ComebackDateController.java
@@ -2,6 +2,7 @@ package com.example.backend;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ public class ComebackDateController {
     @Autowired
 	private ComebackDateRepository repository;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/last-comeback-date")
     public ComebackDate lastComebackDate(){
         return repository.findFirstByOrderByIdDesc();
