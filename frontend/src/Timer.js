34247145ba6076abba1f013d7a61ce72961bb16c
Zhiwei <sherman2zhiwei@yahoo.com>
2021-10-08 19:55:41 +0800

    Add time to Bol4's next comeback

--- frontend/src/Timer.js
+++ frontend/src/Timer.js
@@ -33,7 +33,10 @@ function textToCard(text){
 class Timer extends React.Component {
     constructor(props){
         super(props);
-        this.state = { days: null }
+        this.state = { 
+            days: null,
+            nextComeback: null,
+        }
     }
 
     componentDidMount(){
@@ -42,18 +45,34 @@ class Timer extends React.Component {
 
         const nextComeback=`http://localhost:8080/next-comeback-date?datetime=${encodeURIComponent(dateNow)}`
 
-        console.log(nextComeback)
         fetch(nextComeback)
             .then(response => response.json())
             .then(data => {
-                if (data.datetime == null){
+                console.log(data)
+                if (!data.datetime){
                     this.setState({
                         nextComeback: {}
                     })
                 } else {
                     this.setState({
-                        nextComeback: data
+                        nextComeback: {
+                            name: data.name,
+                            imageData: data.image.data,
+                        }
                     })
+
+                    this.nextComebackInterval = setInterval(() => {
+                        let timeObject = msToDaysHoursMinutes(new Date - new Date(data.datetime))
+    
+                        this.setState({
+                            nextComeback: {
+                                days: timeObject.days,
+                                hours: timeObject.hours,
+                                minutes: timeObject.minutes,
+                                seconds: timeObject.seconds,
+                            }
+                        })
+                    }, 1000)
                 }
             })
             .catch(error => console.log(error))
@@ -82,6 +101,7 @@ class Timer extends React.Component {
 
     componentWillUnmount(){
         clearInterval(this.interval)
+        clearInterval(this.nextComebackInterval)
     }
 
 
@@ -89,9 +109,12 @@ class Timer extends React.Component {
         return <>
             {this.state.days 
                 ? 
-                <div class="h-screen bg-fixed" style={{background: `url('data:image/jpg;base64,${this.state.imageData}')`}}>
+                <div class="h-auto xl:h-full bg-fixed" style={{background: `url('data:image/jpg;base64,${this.state.imageData}')`}}>
                     <div class="bg-black h-full bg-opacity-75 flex flex-col justify-center">
-                        <div class="xl:flex items-center justify-center py-4">
+
+                       <p class="xl:text-center text-5xl mx-5 font-body font-black text-white pb-5 pt-12">Time since Bol4's Last Comeback ({this.state.name}) :</p> 
+         
+                        <div class="xl:flex items-center justify-center">
                             {["days", "hours", "minutes", "seconds"].map(text => {
                                 return (
                                     <div class="flex items-center py-4 text-center">
@@ -104,12 +127,47 @@ class Timer extends React.Component {
                             }
                         </div>
 
-                        <p class="xl:text-center text-5xl mx-5 font-body font-black text-white"> since Bol4's Last Comeback : {this.state.name}</p> 
+                        <div class="h-24">
+
+                        </div>
+
+
+
+                        {
+                        this.state.nextComeback
+                        ? 
+                        <>  
+                            <p class="xl:text-center text-5xl mx-5 font-body font-black text-white py-5"> Time to Bol4's Next Comeback :</p>
+
+                            <div class="xl:flex items-center justify-center pb-4">
+                                {
+                                this.state.nextComeback.days ? 
+                                ["days", "hours", "minutes", "seconds"].map(text => {
+                                    return (
+                                        <div class="flex items-center py-4 text-center">
+                                            {textToCard(this.state.nextComeback[text].toString())}
+                                            <p class="text-5xl font-body font-black text-white"> {text}, </p>
+                                        </div> 
+                                    )
+                                }) : 
+
+                                <div class="px-5 pt-5 pb-12">
+                                    <div class="py-5 px-9 bg-red-500 shadow-md rounded-xl text-center">
+                                        <p class="text-7xl font-medium font-body text-white">UNKNOWN :(</p>
+                                    </div>
+                                </div>
+
+                                }
+                            </div>
+                        </> : null
+                        }
                     </div>
                 </div>
-                    
+                        
                 : null
-            }
+                }
+
+
         </>
     }
 }
